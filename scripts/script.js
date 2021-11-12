ef91da940a0c3100c10a00b1884d269be272776c
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-12 10:41:52 -0300

    checkpoint

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,11 @@ const { ethers } = require("ethers");
 
 module.exports = async () => {
   const simple = await Simple.deployed();
+  const simpleNetwork = simple.networks["80001"].address;
+  console.log(simpleNetwork);
+  const t = await ethers.providers.getDefaultProvider();
+  console.log(t);
+
   const [account, _] = await web3.eth.getAccounts();
   console.log(account);
 };
